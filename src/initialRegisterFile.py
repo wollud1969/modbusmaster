99238720c9a260ba38537051ac30b0641a06173b
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2019-07-08 15:15:16 +0100

    change registers

--- src/initialRegisterFile.py
+++ src/initialRegisterFile.py
@@ -4,9 +4,9 @@ import pickle
 
 
 datapoints = [
-    RegisterDatapoint.InputRegisterDatapoint('Temperature', 5, 0x0001, 1, datetime.timedelta(seconds=0.2), 'Pub/Temperature'),
-    RegisterDatapoint.InputRegisterDatapoint('Humidity', 5, 0x0002, 1, datetime.timedelta(seconds=0.2), 'Pub/Humidity'),
-    RegisterDatapoint.DiscreteInputDatapoint('Switches', 4, 0x0000, 1, datetime.timedelta(seconds=0.2), 'Pub/Switches'),
+    RegisterDatapoint.InputRegisterDatapoint('Temperature', 5, 0x0001, 1, datetime.timedelta(seconds=1.0), 'Pub/Temperature'),
+    RegisterDatapoint.InputRegisterDatapoint('Humidity', 5, 0x0002, 1, datetime.timedelta(seconds=1.0), 'Pub/Humidity'),
+    RegisterDatapoint.DiscreteInputDatapoint('Switches', 4, 0x0000, 1, datetime.timedelta(seconds=1.0), 'Pub/Switches'),
 ]
 
 
